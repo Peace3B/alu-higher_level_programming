110bd0b1ececf6435b1012a6920fd5c173464df8
Peace3B <p.keza@alustudent.com>
2024-02-21 07:56:54 -0800

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    char = chr(i)
-    if i % 2 == 0:  # Even ASCII value
-        char = char.upper()
-    print(char, end="")
+print("".join(chr(122 - i).upper() if i % 2 == 0 else chr(122 - i) for i in range(26)))
 
