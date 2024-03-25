8a36a5f90c684d54cbff3b157e354c028710956c
Peace3B <p.keza@alustudent.com>
2024-03-25 11:09:23 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Fetches http://0.0.0.0:5050/status."""
+"""
+Fetches http://0.0.0.0:5050/status and displays the body response in a specific format.
+"""
 import urllib.request
 
 if __name__ == "__main__":
@@ -7,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
+        print("\t- type: {}".format(type(body).__name__))
+        print("\t- content: {}".format(body.decode('utf-8')))
         print("\t- utf8 content: {}".format(body.decode('utf-8')))
