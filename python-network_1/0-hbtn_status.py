34800849fa01c483fa8a09a51fff7dcfcf95e88f
Peace3B <p.keza@alustudent.com>
2024-03-27 14:47:28 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,14 @@
-#!/usr/bin/python3
-"""Documented by Lsblack"""
+#!usr/bin/python3
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
-    '\n    Chrome/99.0.4844.84 Safari/537.36',
-}
+url = "https://intranet.hbtn.io/status"
 
-req = urllib.request.Request(url, headers=headers)
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+try:
+    with urllib.request.urlopen(url) as response:
+        data = response.read().decode('utf-8')
+
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+except Exception as e:
+    print("Error fetching URL:", e)
